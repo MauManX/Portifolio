7933bcaf0cf793ff3540a0f1d9348df6e6e06ecc
Maurício Horiuchi <maumanx@hotmail.com>
2025-05-29 13:47:48 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+﻿function showPic(_pic){
+    if (_pic != ''){
+        document.getElementById('bigPic').style.visibility = 'visible';
+        document.getElementById('pic').src = _pic;
+    }else{
+        document.getElementById('bigPic').style.visibility = 'hidden';
+    }
+}
